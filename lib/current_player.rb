dd6de09e6956aca96edc4de597886b38e48f601f
Robert McCary <robert.tyler.mccary@gmail.com>
2019-08-24 17:47:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,11 @@ def current_player
 
 end
 
-def turn_count
-
+def turn_count(board_array)
+  first_player = "X"
+  second_player = "O"
+  count = 0
+  if first_player == board_array
+    count += 1
+  end 
 end 
