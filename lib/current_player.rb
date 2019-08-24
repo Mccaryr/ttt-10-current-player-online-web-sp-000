14df51e61cb507bd600757143e1598c112452e62
Robert McCary <robert.tyler.mccary@gmail.com>
2019-08-24 17:48:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,8 +6,7 @@ def turn_count(board_array)
   first_player = "X"
   second_player = "O"
   count = 0
-  if first_player == board_array
+  board_array.each do |move|
     count += 1
-  end
-  return count
+  end 
 end
